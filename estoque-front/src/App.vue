53de2df22b5b0a02c22e33b796407d2405eedd6d
Renan Nunes <renan.nunes@stone.com.br>
2021-02-17 23:57:34 -0300

    Added bootstrap vue and added initial navbar

--- estoque-front/src/App.vue
+++ estoque-front/src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App =)"/>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">Estoque</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item-dropdown href="#" text="Produtos">
+            <b-dropdown-item href="#">Adicionar produto</b-dropdown-item>
+            <b-dropdown-item href="#">Alterar/remover produto</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item href="#">Visualizar estoque</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Login/Log out</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
