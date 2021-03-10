45313c1cd6187549a5058ceb4cff9a4636343d40
Renan Nunes <renan.nunes@stone.com.br>
2021-03-09 22:50:49 -0300

    Changed href to to on the menu

--- estoque-front/src/App.vue
+++ estoque-front/src/App.vue
@@ -16,7 +16,7 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="/login">Login/Log out</b-nav-item>
+          <b-nav-item to="/login">Login/Log out</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
